81cb740377d0f5c2b77d2656cfa0ff333c753d1d
Max <84903084+second2falcon@users.noreply.github.com>
2026-02-11 13:32:46 +0100

    Refactor tax calculation logic and add launch configuration for debugging

--- main.c
+++ main.c
@@ -1,21 +1,26 @@
-#include <stddef.h>
+#include <stdio.h>
 #define SCHIJF1GRENS 38883
 #define SCHIJF1TARIEF 0.3575
 #define SCHIJF2GRENS 78426
 #define SCHIJF2TARIEF 0.3756
 #define SCHIJF3TARIEF 0.495
+
+float belastingBerekenen(int inkomen)
+{
+    if (inkomen < SCHIJF1GRENS) {
+        return inkomen * SCHIJF1TARIEF;
+    } else if (inkomen < SCHIJF2GRENS) {
+        return (SCHIJF1GRENS * SCHIJF1TARIEF) + ((inkomen - SCHIJF1GRENS) * SCHIJF2TARIEF);
+    } else {
+        return (SCHIJF1GRENS * SCHIJF1TARIEF) + ((SCHIJF2GRENS - SCHIJF1GRENS) * SCHIJF2TARIEF) + ((inkomen - SCHIJF2GRENS) * SCHIJF3TARIEF);
+    }
+}
 int main(void)
 {
     int inkomen;
     printf("Inkomen invoeren: \n");
     scanf("%d", &inkomen);
-
-    if (inkomen < SCHIJF1GRENS) {
-        printf("Te betalen belasting: %.2f\n", inkomen * SCHIJF1TARIEF);
-    } else if (inkomen < SCHIJF2GRENS) {
-        printf("Te betalen belasting: %.2f\n", (SCHIJF1GRENS * SCHIJF1TARIEF) + ((inkomen - SCHIJF1GRENS) * SCHIJF2TARIEF));
-    } else {
-        printf("Te betalen belasting: %.2f\n", (SCHIJF1GRENS * SCHIJF1TARIEF) + ((SCHIJF2GRENS - SCHIJF1GRENS) * SCHIJF2TARIEF) + ((inkomen - SCHIJF2GRENS) * SCHIJF3TARIEF));
-    }
+    float belasting = belastingBerekenen(inkomen);
+    printf("De te betalen belasting is: %.2f\n", belasting);
     return 0;
 }
